14091670bab1f8748905bb32fd698803e0d1f86c
Anton Tonev <atonevbg@gmail.com>
2013-09-10 14:01:56 +0300

    Add Docblocks

--- Module.php
+++ Module.php
@@ -1,5 +1,14 @@
 <?php
 /**
+ * Coolcsn Zend Framework 2 Navigation Module
+ * 
+ * @link https://github.com/coolcsn/CsnAclNavigation for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 LightSoft 2005 Ltd. Bulgaria
+ * @license https://github.com/coolcsn/CsnAclNavigation/blob/master/LICENSE BSDLicense
+ * @authors Stoyan Cheresharov <stoyan@coolcsn.com>, Anton Tonev <atonevbg@gmail.com>
+*/
+
+/**
  * This file is placed here for compatibility with ZendFramework 2's ModuleManager.
  * It allows usage of this module even without composer.
  * The original Module.php is in 'src/CsnAclNavigation' in order to respect PSR-0
--- config/module.config.php
+++ config/module.config.php
@@ -1,2 +1,11 @@
 <?php
+/**
+ * Coolcsn Zend Framework 2 Navigation Module
+ * 
+ * @link https://github.com/coolcsn/CsnAclNavigation for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 LightSoft 2005 Ltd. Bulgaria
+ * @license https://github.com/coolcsn/CsnAclNavigation/blob/master/LICENSE BSDLicense
+ * @authors Stoyan Cheresharov <stoyan@coolcsn.com>, Anton Tonev <atonevbg@gmail.com>
+*/
+
 return array();
